2efb12e21a189eb8edcf9b3b483a35c6e56ea8b4
houndhollis <houndhollis9797@gmail.com>
2025-03-25 01:38:42 +0900

    feat: 컴포넌트 분리

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { userAction } from "../action/userAction";
+
+export const Header = () => {
+  const isLogin = userAction.checkIsLoginStatus();
+  const isHome = window.location.pathname === "/";
+  return `
+    <header class="bg-blue-600 text-white p-4 sticky top-0">
+      <h1 class="text-2xl font-bold">항해플러스</h1>
+    </header>
+
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+      <li><a href="/" class="${isHome ? "text-blue-600" : "text-gray-600"} font-bold">홈</a></li>
+      ${isLogin ? `<li><a href="/profile" class="${!isHome ? "text-blue-600" : "text-gray-600"} font-bold">프로필</a></li>` : ""}
+      ${isLogin ? `<li><a id="logout" href="#" class="text-gray-600">로그아웃</a></li>` : `<li><a href="login" class="text-gray-600">로그인</a></li>`}
+      </ul>
+    </nav>
+  `;
+};
---
+++ src/components/PostList.js
@@ -0,0 +1,21 @@
+import { POST_LIST } from "../mock/data";
+
+export const postListHtml = POST_LIST.map(
+  (post) => `
+  <div class="bg-white rounded-lg shadow p-4">
+    <div class="flex items-center mb-2">
+      <img src="https://placehold.co/40" alt="프로필" class="rounded-full mr-2">
+      <div>
+        <p class="font-bold">${post.username}</p>
+        <p class="text-sm text-gray-500">${post.time}</p>
+      </div>
+    </div>
+    <p>${post.content}</p>
+    <div class="mt-2 flex justify-between text-gray-500">
+      <button>좋아요</button>
+      <button>댓글</button>
+      <button>공유</button>
+    </div>
+  </div>
+`,
+).join("");
