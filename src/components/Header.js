2affff383c7272f39ef39ff77eb79c69fa6b6344
houndhollis <houndhollis9797@gmail.com>
2025-03-28 03:01:32 +0900

    style: css 누락 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
-import { routerState } from "../routes/routerManager";
 import { userAction } from "../store/action/userAction";
 
 export const Header = () => {
   const isLogin = userAction.checkIsLoginStatus();
-  const mode = routerState.mode === "history";
 
-  const isAtBaseRoute = mode
-    ? window.location.pathname === (routerState.basePath || "/")
-    : window.location.hash === "#/";
+  const num = location.href.split("/").length;
+  const isProfile = location.href.split("/")[num - 1] === "profile";
 
   return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
@@ -16,8 +13,8 @@ export const Header = () => {
 
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-      <li><a href="/" class="${isAtBaseRoute ? "text-blue-600" : "text-gray-600"} font-bold">홈</a></li>
-      ${isLogin ? `<li><a href="/profile" class="${!isAtBaseRoute ? "text-blue-600" : "text-gray-600"} font-bold">프로필</a></li>` : ""}
+      <li><a href="/" class="${!isProfile ? "text-blue-600" : "text-gray-600"} font-bold">홈</a></li>
+      ${isLogin ? `<li><a href="/profile" class="${isProfile ? "text-blue-600" : "text-gray-600"} font-bold">프로필</a></li>` : ""}
       ${isLogin ? `<li><a id="logout" href="#" class="text-gray-600">로그아웃</a></li>` : `<li><a href="/login" class="text-gray-600">로그인</a></li>`}
       </ul>
     </nav>
